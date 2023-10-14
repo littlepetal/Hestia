e94b9ee3880726cfe30050707fb1b04b7ca86d89
littlepetal <79516085+littlepetal@users.noreply.github.com>
2023-10-14 21:53:49 +1100

    Modified comments and renamed the main file

--- Bush.cpp
+++ Bush.cpp
@@ -1,8 +1,3 @@
-// This program simulates the high level functionalities of a bushfire manager bot
-// The bot can perform controlled burning and fire elimination
-// It is intended that this program is extended to include the use of OpenCV and
-// ROS nodes to implement the simulations on the physical turtlebot
-
 //--Includes-----------------------------------------------------------
 #include "Bush.h"
 
